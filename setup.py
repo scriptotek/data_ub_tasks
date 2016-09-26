e33b82413b8e243d85c4a66450ebba9451089bee
Dan Michael O. Heggø <danmichaelo@gmail.com>
2016-09-26 18:21:24 +0200

    Bump version to 2.5.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='data_ub_tasks',
-      version='2.4.0',
+      version='2.5.1',
       packages=['data_ub_tasks'],
       install_requires=['rdflib', 'SPARQLWrapper', 'requests']
       )
