2918e7a13684992a4f1005be8ae689d19d20cef2
Dan Michael O. Heggø <danmichaelo@gmail.com>
2017-06-26 12:28:35 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from distutils.core import setup
 
 setup(name='data_ub_tasks',
-      version='2.5.4',
+      version='2.5.5',
       packages=['data_ub_tasks'],
-      install_requires=['rdflib', 'SPARQLWrapper', 'requests', 'elasticsearch']
+      install_requires=[
+      	'rdflib',
+      	'SPARQLWrapper',
+      	'requests',
+      	'elasticsearch',
+      	'skosify',
+      	],
+      	dependency_links = ['https://github.com/NatLibFi/Skosify/tarball/master#egg=skosify-2.0.0beta']
       )
